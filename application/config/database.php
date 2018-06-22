df1eb43672e83726fb4b325611fe9177fac80058
sum <xunwu659@live.com>
2018-06-22 19:38:52 +0800

    数据库地址

--- application/config/database.php
+++ application/config/database.php
@@ -75,11 +75,11 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => '',
-	'password' => '',
-	'database' => '',
-	'dbdriver' => 'mysqli',
+	'hostname' => 'www.sghaotong.cn,8085',
+	'username' => 'php',
+	'password' => 'Horoad316116',
+	'database' => 'shaoguanbase',
+	'dbdriver' => 'sqlsrv',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
